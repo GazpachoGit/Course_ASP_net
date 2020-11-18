b1c87f62ec1cba48bd015323b1939eef1479c4b6
Egor <egtornikov@gmail.com>
2020-11-18 16:43:09 +0300

    add a client-webpack staff

---
+++ TicketService/Client/js/site.js
@@ -0,0 +1,15 @@
+﻿import 'bootstrap';
+import 'jquery';
+import 'bootstrap-select';
+
+// CSS Dependencies
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-select/dist/css/bootstrap-select.css';
+
+// Custom JS imports
+import '../js/eventsApp.js'
+
+// Custom CSS imports
+import '../css/events.css';
+
+
